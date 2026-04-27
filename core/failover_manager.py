e7e69aefe409be0a7d31d59d987cd5f40e3406c5
w1boost18 <63419938+w1boost1889M@users.noreply.github.com>
2026-04-27 17:44:57 +0800

    Update failover_manager.py

--- core/failover_manager.py
+++ core/failover_manager.py
@@ -1,7 +1,7 @@
 # =============================================================================
 # Anti-Jam CCTV Protection System
 # =============================================================================
-# Copyright (C) 2026 Avoceous (https://github.com/Avoceous)
+# Copyright (C) 2026 w1boost1889M (https://github.com/w1boost1889M)
 #
 # This file is part of Anti-Jam CCTV Protection System.
 #
@@ -19,8 +19,8 @@
 # this program. If not, see <https://www.gnu.org/licenses/>.
 #
 # Author   : Avoceous
-# GitHub   : https://github.com/Avoceous
-# Project  : https://github.com/Avoceous/anti-jam-cctv
+# GitHub   : https://github.com/w1boost1889M
+# Project  : https://github.com/w1boost1889M/anti-jam-cctv
 # License  : GNU General Public License v3.0 (GPL-3.0)
 # Created  : April 2026
 # File     : core/failover_manager.py
@@ -270,7 +270,7 @@ class FailoverManager:
 
 # =============================================================================
 # End of file: core/failover_manager.py
-# Copyright (C) 2026 Avoceous (https://github.com/Avoceous)
+# Copyright (C) 2026 w1boost1889M (https://github.com/w1boost1889M)
 # Licensed under GNU General Public License v3.0 (GPL-3.0)
-# https://github.com/Avoceous/anti-jam-cctv
+# https://github.com/w1boost1889M/anti-jam-cctv
 # =============================================================================
