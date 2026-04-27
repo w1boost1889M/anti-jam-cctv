c1bbcbc1cbf4b12f0513a06716e81d5b87c0bd8c
w1boost18 <63419938+w1boost1889M@users.noreply.github.com>
2026-04-27 17:43:32 +0800

    Update web_ui.py

--- dashboard/web_ui.py
+++ dashboard/web_ui.py
@@ -1,7 +1,7 @@
 # =============================================================================
 # Anti-Jam CCTV Protection System
 # =============================================================================
-# Copyright (C) 2026 Avoceous (https://github.com/Avoceous)
+# Copyright (C) 2026 w1boost1889M (https://github.com/w1boost1889M)
 #
 # This file is part of Anti-Jam CCTV Protection System.
 #
